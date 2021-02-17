585d74ca35fd144f55ce9d1dc5693aa6849548c7
Jakub Radzik <jradzik4@gmail.com>
2021-02-17 16:48:38 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from "./Components/Header/Header";
-import React,{useEffect, useState, useRef} from "react";
-import Overview from "./Components/Overview/Overview";
-import styled from "styled-components";
 
 function App() {
-
-    const Diva = styled.div`
-      width: 100%;
-      height: 100vh;
-      background: yellow;
-    `
-
-    const Divb = styled.div`
-      width: 100%;
-      height: 100vh;
-      background: deepskyblue;
-    `
-
-
-    return (
-      <div className="App">
-          <Overview/>
-          <Header/>
-          <Diva/>
-          <Divb/>
-      </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
-
 
 export default App;
