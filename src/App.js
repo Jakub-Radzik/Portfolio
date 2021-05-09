c56b8818e4def109e252a733106ea92cd6108115
Jakub Radzik <jradzik4@gmail.com>
2021-05-09 10:44:49 +0200

    sticky navbar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import './App.css';
 import Header from "./Components/Header/Header";
-import React,{useEffect, useState, useRef} from "react";
+import React from "react";
 import Overview from "./Components/Overview/Overview";
-import styled from "styled-components";
 
 function App() {
-
-    const Diva = styled.div`
-      width: 100%;
-      height: 100vh;
-      background: yellow;
-    `
-
-    const Divb = styled.div`
-      width: 100%;
-      height: 100vh;
-      background: deepskyblue;
-    `
-
-
-    return (
+  return (
       <div className="App">
           <Overview/>
           <Header/>
-          <Diva/>
-          <Divb/>
+          <Overview/>
+          <Overview/>
       </div>
   );
 }
-
-
 
 export default App;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,53 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component} from 'react';
 import SiteLogo from "./SiteLogo";
 import Menu from "./Menu";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import Navigation from "react-sticky-nav";
 
-function Header() {
-
-    function logit() {
-        if ((window.innerHeight+500) <= window.pageYOffset) {
-            document.querySelector("#header").classList.add('animationHeightChangerSmall');
-            document.querySelector("#header").classList.remove('animationHeightChangerBig');
-        } else {
-            document.querySelector("#header").classList.add('animationHeightChangerBig');
-            document.querySelector("#header").classList.remove('animationHeightChangerSmall');
-        }
-        // console.log("wH:", window.innerHeight)
-        // console.log(window.pageYOffset);
-    }
-
-    useEffect(() => {
-        function watchScroll() {
-            window.addEventListener("scroll", logit);
-        }
-        watchScroll();
-        return () => {
-            window.removeEventListener("scroll", logit);
-        };
-
-    }, []);
+function Header(){
 
     const Header = styled(Navigation)`
-      background-color: #C1292E;
-      position: -webkit-sticky;
-      display: flex;
-      align-items: center;
+        background-color: #2727b1;
     `;
 
     const Responsive = styled.nav`
-      margin: auto;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-around;
-      align-items: center;
-      max-width: 1900px;
-      width: 100%;
+          margin: auto;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-around;
+          align-items: center;
+          max-width: 1900px;
+          width: 100%;
     `;
 
     return (
-        <Header id={"header"}>
+        <Header>
             <Responsive id="responsive">
                 <SiteLogo/>
                 <Menu/>
